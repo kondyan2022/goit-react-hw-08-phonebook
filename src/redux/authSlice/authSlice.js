685ee68974b128a36c1e6c66959d33fd19df1b8c
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-07-31 01:59:19 +0300

    refactoring AuthSlice

--- src/redux/authSlice/authSlice.js
+++ src/redux/authSlice/authSlice.js
@@ -6,6 +6,16 @@ import {
   logoutUserThunk,
   refreshUserThunk,
 } from './thunk';
+import {
+  authHandleFulfilled,
+  authHandleFulfilledLoggedIn,
+  authHandleFulfilledLoggedOut,
+  authHandleFulfilledRefresh,
+  authHandlePending,
+  authHandlePendingRefresh,
+  authHandleRejected,
+  authHandleRejectedRefresh,
+} from './handleAuth';
 
 const STATUS = {
   PENDING: 'pending',
@@ -15,63 +25,22 @@ const STATUS = {
 const arrThunk = [signupUserThunk, loginUserThunk, logoutUserThunk];
 const getArrThunkStatus = status => arrThunk.map(elem => elem[status]);
 
-const authHandlePending = state => {
-  state.isPending = true;
-};
-
-const authHandleFulfilled = state => {
-  state.isPending = false;
-};
-const authHandleFulfilledLoggedIn = (state, { payload }) => {
-  state.user = payload.user;
-  state.token = payload.token;
-  state.isLoggedIn = true;
-};
-
-const authHandleFulfilledLoggedOut = state => {
-  state.user = { name: null, email: null };
-  state.token = null;
-  state.isLoggedIn = false;
-};
-
-const authHandleRejected = state => {
-  state.isPending = false;
-};
-const authHandlePendingRefresh = state => {
-  state.isRefreshing = true;
-};
-const authHandleRejectedRefresh = state => {
-  state.isRefreshing = false;
-};
-const authHandleFulfilledRefresh = (state, { payload }) => {
-  state.isRefreshing = false;
-  state.isLoggedIn = true;
-  state.user = payload;
-};
-
 export const authSlice = createSlice({
   name: 'auth',
   initialState: initialState,
   extraReducers: builder => {
     const { PENDING, FULFILLED, REJECTED } = STATUS;
     builder
-      // .addCase(signupUserThunk.pending, authHandlePending)
-      // .addCase(loginUserThunk.pending, authHandlePending)
-      // .addCase(logoutUserThunk.pending, authHandlePending)
-      // .addCase(loginUserThunk.fulfilled, authHandleFulfilled)
-      // .addCase(signupUserThunk.fulfilled, authHandleFulfilled)
-      // .addCase(logoutUserThunk.fulfilled, authHandleFulfilled)
-      // .addCase(signupUserThunk.rejected, authHandleRejected)
-      // .addCase(loginUserThunk.rejected, authHandleRejected)
-      // .addCase(logoutUserThunk.rejected, authHandleRejected)
-      .addCase(signupUserThunk.fulfilled, authHandleFulfilledLoggedIn)
-      .addCase(loginUserThunk.fulfilled, authHandleFulfilledLoggedIn)
       .addCase(logoutUserThunk.fulfilled, authHandleFulfilledLoggedOut)
       .addCase(refreshUserThunk.pending, authHandlePendingRefresh)
       .addCase(refreshUserThunk.fulfilled, authHandleFulfilledRefresh)
       .addCase(refreshUserThunk.rejected, authHandleRejectedRefresh)
       .addMatcher(isAnyOf(...getArrThunkStatus(PENDING)), authHandlePending)
       .addMatcher(isAnyOf(...getArrThunkStatus(FULFILLED)), authHandleFulfilled)
-      .addMatcher(isAnyOf(...getArrThunkStatus(REJECTED)), authHandleRejected);
+      .addMatcher(isAnyOf(...getArrThunkStatus(REJECTED)), authHandleRejected)
+      .addMatcher(
+        isAnyOf(signupUserThunk.fulfilled, loginUserThunk.fulfilled),
+        authHandleFulfilledLoggedIn
+      );
   },
 });
